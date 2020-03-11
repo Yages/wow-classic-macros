2e31f860b70e139471c38f503370c39dc592c804
Lucas Bradd <lucas@bradd.com.au>
2020-03-11 17:21:45 +1100

    Added mana-gem macro.

---
+++ mage/mana-gems.lua
@@ -0,0 +1,6 @@
+/castsequence [nocombat] reset=120 Conjure Mana Ruby,Conjure Mana Citrine,Conjure Mana Jade
+#showtooltip
+/use Robe of the Archmage 
+/use Mana Ruby
+/use Mana Citrine
+/use Mana Jade
