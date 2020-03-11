f34fdba110c29ec37953cb5fc09812da620e54e4
Lucas Bradd <lucas@bradd.com.au>
2020-03-11 17:38:10 +1100

    Updated mana-gems macro for betterer tooltop.

--- mage/mana-gems.lua
+++ mage/mana-gems.lua
@@ -1,5 +1,5 @@
+#showtooltip Mana Ruby
 /castsequence [nocombat] reset=120 Conjure Mana Ruby,Conjure Mana Citrine,Conjure Mana Jade
-#showtooltip
 /use Robe of the Archmage 
 /use Mana Ruby
 /use Mana Citrine
